38e4193b711ebed39fdaba0be32351f4bfddb1fe
Loïc Boutter <loic.boutter@axiocode.com>
2025-12-13 09:53:23 +0100

    2025 / day 8 / part 2 (way easier than part 1)

--- 2025/day08.lua
+++ 2025/day08.lua
@@ -9,7 +9,7 @@ Day.config = {
   name = "Playground",
   expected = {
     part1 = 40,
-    part2 = nil,
+    part2 = 25272,
   },
 }
 
@@ -97,6 +97,38 @@ local function make_circuits(boxes, boxes_distances, nb_connections)
   return circuits, circuits_lengths
 end
 
+local function make_all_circuits(boxes, boxes_distances)
+  local circuits = {}
+  local circuits_indices = {}
+  for _, box in pairs(boxes) do
+    local xyz = coords_to_index(box)
+    circuits_indices[xyz] = xyz
+    circuits[xyz] = { xyz }
+  end
+
+  local last_junction_boxes = {}
+  for i = 1, #boxes_distances do
+    local distance = boxes_distances[i]
+    local box1 = distance.box1
+    local box2 = distance.box2
+    local i1 = circuits_indices[box1]
+    local i2 = circuits_indices[box2]
+
+    if i1 ~= i2 then
+      local circuit1 = circuits[i1]
+      local circuit2 = circuits[i2]
+      while #circuit2 > 0 do
+        local item = table.remove(circuit2, 1)
+        table.insert(circuit1, item)
+        circuits_indices[item] = i1
+      end
+      last_junction_boxes = { box1, box2 }
+    end
+  end
+
+  return last_junction_boxes
+end
+
 -- Part 1 solution
 function Day.part1(data, is_test)
   local nb_connections = is_test and 10 or 1000
@@ -112,9 +144,15 @@ end
 
 -- Part 2 solution
 function Day.part2(data, is_test)
-  -- Implement part 2 here
-  local parsed = parse_input(data)
-  return 0
+  local boxes = parse_input(data)
+  local boxes_distances = compute_distances(boxes)
+
+  local last_junction_boxes = make_all_circuits(boxes, boxes_distances)
+
+  local x1 = utils.split(last_junction_boxes[1], ",")[1]
+  local x2 = utils.split(last_junction_boxes[2], ",")[1]
+
+  return x1 * x2
 end
 
 return require("lib.runner").run(Day)
